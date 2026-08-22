1e6a848dbc69bb27ab9df4ba71ad84c104aeafda
Jesús Hernández <yisus.tarnished@gmail.com>
2026-08-21 18:06:03 -0600

    Se crea CafeRequest para capa DTO

--- src/main/java/com/jecsamtech/kapebackend/dto/CafeRequest.java
+++ src/main/java/com/jecsamtech/kapebackend/dto/CafeRequest.java
@@ -1,4 +1,10 @@
 package com.jecsamtech.kapebackend.dto;
+import lombok.Data;
+import jakarta.validation.constraints.*;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 public class CafeRequest {
+
 }
