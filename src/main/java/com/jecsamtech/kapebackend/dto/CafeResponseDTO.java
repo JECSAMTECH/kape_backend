6731a47bb5fc6ea9d99ca9ebc980d920c04436c8
Jesús Hernández <yisus.tarnished@gmail.com>
2026-08-24 12:22:15 -0600

    Se agrega response en dto de Cafe

--- src/main/java/com/jecsamtech/kapebackend/dto/CafeResponseDTO.java
+++ src/main/java/com/jecsamtech/kapebackend/dto/CafeResponseDTO.java
@@ -1,4 +1,16 @@
 package com.jecsamtech.kapebackend.dto;
 
+import java.math.BigDecimal;
+
 public class CafeResponseDTO {
+    private Long idCafe;
+    private String nombreCafe;
+    private String descripcionCafe;
+    private String imagenCafe;
+    private String etiquetasCafe;
+    private String tuesteCafe;
+    private String notasCataCafe;
+    private Integer intensidadCafe;
+    private BigDecimal precioCafe;
+    private Integer stockCafe;
 }
