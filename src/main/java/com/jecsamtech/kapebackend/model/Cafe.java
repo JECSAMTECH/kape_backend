1a9ca2399b50dc6fccd4329b0c272f1344ac2876
Jesús Hernández <yisus.tarnished@gmail.com>
2026-08-24 12:13:10 -0600

    Se quitan validaciones del model para pasarlas a DTO y tener entidad más limpia

--- src/main/java/com/jecsamtech/kapebackend/model/Cafe.java
+++ src/main/java/com/jecsamtech/kapebackend/model/Cafe.java
@@ -21,45 +21,31 @@ public class Cafe {
     @Column(name = "id_cafe")
     private Long idCafe;
 
-    @NotBlank(message = "El nombre del producto no puede estar vacío")
     @Column(name = "nombre", nullable = false, length = 45)
     private String nombreCafe;
 
-    @NotBlank(message = "La descripción no puede ir vacia")
     @Column(name = "descripcion", nullable = false, length = 125)
     private String descripcionCafe;
 
     //Atributo imagen
-    @NotBlank(message = "La imagen es obligatoria")
     @Column(name = "imagen", nullable = false, length = 2048)
     private String imagenCafe;
 
-    @NotBlank(message = "Campo etiquetas no puede ir vacio o ser nulo")
     @Column(name = "etiquetas", nullable = false, length = 120)
     private String etiquetasCafe;
 
-    @NotBlank(message = "El tueste no puede ir vacio")
     @Column(name = "tueste", nullable = false, length = 45)
     private String tuesteCafe;
 
-    @NotBlank(message = "La nota_cata no puede ir vacia")
     @Column(name = "notas_de_cata", nullable = false, length = 125)
     private String notasCataCafe;
 
-    @NotNull(message = "La intensidad no puede ser nulo o vacio")
-    @Positive(message = "Debe ser mayor que cero la intensidad")
-    @Max(value = 3, message = "No puede ser mayor a 3 la intensidad")
     @Column( name = "intensidad", nullable = false)
     private Integer intensidadCafe;
 
-
-    @NotNull(message = "El percio no puede ir vacio")
-    @PositiveOrZero(message = "Debe ser mayor o igual que cero")
     @Column(name = "precio", nullable = false, precision = 10, scale = 2)
     private BigDecimal precioCafe;
 
-    @NotNull(message = "Stock del producto no puede estar vacio")
-    @PositiveOrZero(message = "Debe ser igual o mayor que cero")
     @Column(name = "stock", nullable = false)
     private Integer stockCafe;
 }
